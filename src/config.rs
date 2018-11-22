ade9b6e24aa35965e2eed316012790d6584d7a46
esneider <dariosn@gmail.com>
2018-11-22 13:53:55 -0300

    Accept URIs for the daemon_rpc_addr

--- src/config.rs
+++ src/config.rs
@@ -21,7 +21,7 @@ pub struct Config {
     pub network_type: Network,
     pub db_path: PathBuf,
     pub daemon_dir: PathBuf,
-    pub daemon_rpc_addr: SocketAddr,
+    pub daemon_rpc_addr: String,
     pub cookie: Option<String>,
     pub electrum_rpc_addr: SocketAddr,
     pub monitoring_addr: SocketAddr,
@@ -171,13 +171,11 @@ impl Config {
             Network::Regtest => 24224,
         };
 
-        let daemon_rpc_addr: SocketAddr = str_to_socketaddr(
-            m.value_of("daemon_rpc_addr").unwrap_or(&format!(
-                "{}:{}",
-                DEFAULT_SERVER_ADDRESS, default_daemon_port
-            )),
-            "Bitcoin RPC",
-        );
+        let daemon_rpc_addr: String = m
+            .value_of("daemon_rpc_addr")
+            .unwrap_or(&format!("127.0.0.1:{}", default_daemon_port))
+            .parse()
+            .expect("invalid Bitcoind RPC address");
         let electrum_rpc_addr: SocketAddr = str_to_socketaddr(
             m.value_of("electrum_rpc_addr").unwrap_or(&format!(
                 "{}:{}",
